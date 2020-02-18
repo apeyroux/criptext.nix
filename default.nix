fb63a076d09dca14d557d5745c477db5b36ee662
Alexandre Peyroux <alex@px.io>
2020-02-18 22:09:08 +0100

    up to 0.27.1

--- default.nix
+++ default.nix
@@ -2,18 +2,22 @@ with import <nixpkgs> {};
 
 let
 
-  version = "0.26.1";
+  version = "0.27.1";
 
   criptext-app = fetchurl {
     url = "https://cdn.criptext.com/Criptext-Email-Desktop/linux/Criptext-latest.AppImage";
-    sha256 = "0p7zqjw4ra7h0jfsra2vcv3vn9jny77i81y90dvwvavs22w6hi1l";
+    sha256 = "118l907wd6xf88m6d599a1draw4bgm2mxx4d6c35apgiyzyq23dy";
     executable = true;
   };
 
+  apprun = appimage-run.override {
+    extraPkgs = pkgs: [libsecret];
+  };
+  
   criptext-bin = writeScriptBin "criptext" ''
     #!${pkgs.stdenv.shell}
 
-    XDG_DATA_DIRS=${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS ${appimage-run}/bin/appimage-run ${criptext-app}
+    XDG_DATA_DIRS=${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS ${apprun}/bin/appimage-run ${criptext-app}
   '';
 
 in criptext-bin
