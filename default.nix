f8834a9bebebd4083fe00615fcf54de990a78fc7
Alexandre Peyroux <alex@px.io>
2020-05-12 00:21:18 +0200

    criptext: 0.27.1 -> 0.28.5

--- default.nix
+++ default.nix
@@ -2,22 +2,28 @@ with import <nixpkgs> {};
 
 let
 
-  version = "0.27.1";
+  version = "0.28.5";
 
   criptext-app = fetchurl {
     url = "https://cdn.criptext.com/Criptext-Email-Desktop/linux/Criptext-latest.AppImage";
-    sha256 = "118l907wd6xf88m6d599a1draw4bgm2mxx4d6c35apgiyzyq23dy";
+    sha256 = "1ywjbnksdv8hi71bz7x3q1mpi3vxyfqqpwz451y9qy48mwwfizha";
     executable = true;
   };
 
   apprun = appimage-run.override {
-    extraPkgs = pkgs: [libsecret];
+    extraPkgs = pkgs: [
+      libsecret
+      sqlite
+      
+      corefonts
+      dejavu_fonts
+    ];
   };
   
   criptext-bin = writeScriptBin "criptext" ''
     #!${pkgs.stdenv.shell}
 
-    XDG_DATA_DIRS=${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS ${apprun}/bin/appimage-run ${criptext-app}
+    XDG_DATA_DIRS=${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS FONTCONFIG_FILE=${pkgs.fontconfig.out}/etc/fonts/fonts.conf ${apprun}/bin/appimage-run ${criptext-app}
   '';
 
 in criptext-bin
